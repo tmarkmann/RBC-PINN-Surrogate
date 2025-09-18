27f9ceefbc8786665ada4b479f0753557eef5246
tmarkmann <tmarkmann@techfak.uni-bielefeld.de>
2025-09-18 12:36:40 +0200

    Start LRAN3D impl

---
+++ scripts/train_lran_3D.py
@@ -0,0 +1,70 @@
+import hydra
+import lightning as L
+from lightning.pytorch.callbacks import (
+    EarlyStopping,
+    RichModelSummary,
+    RichProgressBar,
+)
+from lightning.pytorch.loggers import WandbLogger
+from omegaconf import DictConfig
+from rbc_pinn_surrogate.data import RBCDatamodule3D
+from rbc_pinn_surrogate.model import LRANModule
+from rbc_pinn_surrogate.callbacks import (
+    Metrics3DCallback,
+)
+
+
+@hydra.main(version_base="1.3", config_path="../configs", config_name="lran3D")
+def main(config: DictConfig):
+    # seed
+    L.seed_everything(config.seed, workers=True)
+    
+    # data
+    dm = RBCDatamodule3D(**config.data)
+    dm.setup("fit")
+
+    # model
+    denormalize = dm.datasets["train"].denormalize_batch
+    model = LRANModule(**config.model, inv_transform=denormalize)
+
+    # logger
+    logger = WandbLogger(
+        entity="sail-project",
+        project="RBC-3D-LRAN",
+        save_dir=config.paths.output_dir,
+        log_model=False,
+    )
+
+    # callbacks
+    callbacks = [
+        RichProgressBar(),
+        RichModelSummary(),
+        EarlyStopping(
+            monitor="val/loss",
+            mode="min",
+            patience=8,
+        ),
+        Metrics3DCallback(),
+    ]
+
+    # trainer
+    trainer = L.Trainer(
+        logger=logger,
+        accelerator="auto",
+        default_root_dir=config.paths.output_dir,
+        check_val_every_n_epoch=2,
+        log_every_n_steps=10,
+        max_epochs=config.algo.epochs,
+        callbacks=callbacks,
+        enable_checkpointing=False,
+    )
+
+    # training
+    trainer.fit(model, dm)
+
+    # rollout on test set
+    #trainer.test(model, datamodule=dm, ckpt_path="best")
+
+
+if __name__ == "__main__":
+    main()
