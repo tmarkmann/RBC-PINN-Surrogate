cae5273a45d33f056441702372c335e932f010ab
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2025-09-25 00:21:19 +0200

    Add 3d results

---
+++ scripts/download_3d_results.py
@@ -0,0 +1,42 @@
+import json
+import wandb
+import pandas as pd
+import os
+
+
+def get_sweep_results(project, method, tag):
+    # set your entity and project
+    api = wandb.Api()
+    runs = api.runs(project, filters={"tags": {"$in": [tag]}})
+
+    root = "notebooks/results"
+    os.makedirs(root, exist_ok=True)
+
+    # Download
+    for run in runs:
+        ra = run.config["ra"]
+        seed = run.config["seed"]
+        for artifact in run.logged_artifacts():
+            if artifact.type == "run_table" and "Table-Metrics" in artifact.name:
+                # get artifact
+                table_dir = artifact.download(root=f"{root}/artifacts/{run.id}")
+                table_path = f"{table_dir}/test/Table-Metrics.table.json"
+                # convert json to pd
+                with open(table_path) as file:
+                    json_dict = json.load(file)
+                df = pd.DataFrame(json_dict["data"], columns=json_dict["columns"])
+                # write as csv
+                outdir = f"{root}/{method}"
+                os.makedirs(outdir, exist_ok=True)
+                df.to_csv(f"{outdir}/ra{ra}-{seed}.csv", index=False)
+
+# config
+sweeps = [
+    "sail-project/RBC-3D-FNO",
+]
+methods = [
+    "3d-fno",
+]
+
+for sweep, method in zip(sweeps, methods):
+    get_sweep_results(sweep, method, tag="results1")
