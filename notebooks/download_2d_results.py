834913786d32680a43945e31e624789bd8db81c1
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2025-12-30 17:43:04 +0100

    Add superres notebooks

--- notebooks/download_2d_results.py
+++ notebooks/download_2d_results.py
@@ -2,6 +2,36 @@ import json
 import wandb
 import pandas as pd
 import os
+from pathlib import Path
+
+ROOT = str(Path(__file__).resolve().parent)
+
+def get_superres_results(sweep, method):
+    # set your entity and project
+    api = wandb.Api()
+    sweep = api.sweep(sweep)
+    runs = sweep.runs
+
+    root = ROOT + "/results"
+    os.makedirs(root, exist_ok=True)
+
+    # Download
+    for run in runs:
+        sr = run.config["train_downsample"]
+        seed = run.config["seed"]
+        for artifact in run.logged_artifacts():
+            if artifact.type == "run_table" and "Table-Metrics" in artifact.name:
+                # get artifact
+                table_dir = artifact.download(root=f"{root}/artifacts/{run.id}")
+                table_path = f"{table_dir}/test/Table-Metrics.table.json"
+                # convert json to pd
+                with open(table_path) as file:
+                    json_dict = json.load(file)
+                df = pd.DataFrame(json_dict["data"], columns=json_dict["columns"])
+                # write as csv
+                outdir = f"{root}/{method}"
+                os.makedirs(outdir, exist_ok=True)
+                df.to_csv(f"{outdir}/sr{sr}-{seed}.csv", index=False)
 
 
 def get_sweep_results(sweep, method):
@@ -10,7 +40,7 @@ def get_sweep_results(sweep, method):
     sweep = api.sweep(sweep)
     runs = sweep.runs
 
-    root = "notebooks/results"
+    root = ROOT + "/results"
     os.makedirs(root, exist_ok=True)
 
     # Download
@@ -32,17 +62,19 @@ def get_sweep_results(sweep, method):
                 df.to_csv(f"{outdir}/ra{ra}-{seed}.csv", index=False)
 
 
-# config
-sweeps = [
-    "sail-project/RBC-2D-FNO/fifkw3tu",
-    "sail-project/RBC-2D-FNO/jg6h5fco",
-    "sail-project/RBC-2D-LRAN/mzly1064",
-]
-methods = [
-    "2d-fno2d",
-    "2d-fno3d",
-    "2d-lran",
-]
+# Download Result runs
+# sweeps = [
+#     "sail-project/RBC-2D-FNO/fifkw3tu",
+#     "sail-project/RBC-2D-FNO/jg6h5fco",
+#     "sail-project/RBC-2D-LRAN/mzly1064",
+# ]
+# methods = [
+#     "2d-fno2d",
+#     "2d-fno3d",
+#     "2d-lran",
+# ]
+# for sweep, method in zip(sweeps, methods):
+#    get_sweep_results(sweep, method)
 
-for sweep, method in zip(sweeps, methods):
-    get_sweep_results(sweep, method)
+# Download Superres runs
+get_superres_results("sail-project/RBC-2D-FNO/g4damf96", "2d-fno-superres")
--- notebooks/download_3d_results.py
+++ notebooks/download_3d_results.py
@@ -2,6 +2,37 @@ import json
 import wandb
 import pandas as pd
 import os
+from pathlib import Path
+
+ROOT = str(Path(__file__).resolve().parent)
+
+
+def get_superres_results(project, method):
+    # set your entity and project
+    api = wandb.Api()
+    srs = [f"sr{i}" for i in [1, 2, 4, 8, 16]]
+    runs = api.runs(project, filters={"tags": {"$in": srs}})
+
+    root = ROOT + "/results"
+    os.makedirs(root, exist_ok=True)
+
+    # Download
+    for run in runs:
+        sr = run.config["sr"]
+        seed = run.config["seed"]
+        for artifact in run.logged_artifacts():
+            if artifact.type == "run_table" and "Table-Metrics" in artifact.name:
+                # get artifact
+                table_dir = artifact.download(root=f"{root}/artifacts/{run.id}")
+                table_path = f"{table_dir}/test/Table-Metrics.table.json"
+                # convert json to pd
+                with open(table_path) as file:
+                    json_dict = json.load(file)
+                df = pd.DataFrame(json_dict["data"], columns=json_dict["columns"])
+                # write as csv
+                outdir = f"{root}/{method}"
+                os.makedirs(outdir, exist_ok=True)
+                df.to_csv(f"{outdir}/sr{sr}-{seed}.csv", index=False)
 
 
 def get_sweep_results(project, method, tag):
@@ -50,13 +81,16 @@ def get_sweep_results(project, method, tag):
             df.to_csv(f"{outdir}/ra{ra}-{seed}.csv", index=False)
 
 
-# config
-sweeps = [
-    "sail-project/RBC-3D-FNO",
-    "sail-project/RBC-3D-LRAN",
-    "sail-project/RBC-3D-LSTM",
-]
-methods = ["3d-fno", "3d-lran", "3d-lstm"]
+# Download Result runs
+# sweeps = [
+#     "sail-project/RBC-3D-FNO",
+#     "sail-project/RBC-3D-LRAN",
+#     "sail-project/RBC-3D-LSTM",
+# ]
+# methods = ["3d-fno", "3d-lran", "3d-lstm"]
+# 
+# for sweep, method in zip(sweeps, methods):
+#     get_sweep_results(sweep, method, tag="final_result")
 
-for sweep, method in zip(sweeps, methods):
-    get_sweep_results(sweep, method, tag="final_result")
+# Download Superres runs
+get_superres_results("sail-project/RBC-3D-FNO", "3d-fno-superres")
